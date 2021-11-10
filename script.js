1b26d8b72679c7b9b47343fd79ef32d01fc7a800
Ben Jordan <ben254378@gmail.com>
2021-11-10 13:42:42 -0800

    added calculator functionality with keyboard and float support

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // Ben Jordan
 
-const add = function(x, y) {
+function add(x, y) {
     return x + y;
   };
   
-const subtract = function(x, y) {
+function subtract(x, y) {
     return x-y;
 };
 
-const multiply = function(x, y) {
+function multiply(x, y) {
     return x * y
 };
 
-const divide = function(x, y){
+function divide(x, y){
     return x/y;
 }
 
-const power = function(base, exponent) {
+function power(base, exponent) {
     return base ** exponent;
 };
 
-const factorial = function(num) {
+function factorial(num) {
     let res = 1;
 for(let i=2; i<= num; i++){
     res *= i;
@@ -28,25 +28,23 @@ for(let i=2; i<= num; i++){
 return res;
 };
   
-const operate = function(x, y, operator){
+function operate(x, y, operator){ // make return string?
     let res;
     switch(operator){
-        case "+":{
+        case "+":
             res = add(x, y);
             break;
-        }
-        case "-":{
+        case "-":
             res = subtract(x, y);
             break;
-        }
-        case "*":{
+        case "*":
             res = multiply(x, y);
             break;
-        }
-        case "/":{
+        case "/":
             res = divide(x, y);
             break;
-        }
+        case "^":
+            res = power(x, y);
     }
     return res;
 }
@@ -54,15 +52,20 @@ const operate = function(x, y, operator){
 // generate buttons on calc
 // 0-9, +, -, *, /, enter
 
+addEventListener("keydown", (e) => processValue(e));
 const LEFT = document.querySelector(".left-buttons");
 const RIGHT = document.querySelector(".right-buttons");
+const SCREEN = document.querySelector(".display");
 
-const keys = generateButtons();
-const leftKeys = keys[0]
-const rightKeys = keys[1];
+let ans;
+let firstOperand = "";
+let secondOperand = "";
+let display = "";
+let operator = false;
+let compound = false;
 
 function generateButtons(){
-    let keys = [[["^2", "7", "8", "9"], ["!", "4", "5", "6"], ["C", "1", "2", "3"], ["AC", "0", ".", "="]], ["/", "*", "-", "+"]];
+    let keys = [[["^x", "7", "8", "9"], ["!", "4", "5", "6"], ["C", "1", "2", "3"], ["AC", "0", ".", "="]], ["/", "*", "-", "+"]];
 
     // left button panel
     for(let i = 0; i < 4; i++){ // add four buttons to each row
@@ -72,8 +75,9 @@ function generateButtons(){
             let button = document.createElement("div");
             button.classList.add("button");
             button.innerHTML = keys[0][i][j];
+            button.dataset.value = keys[0][i][j]; // use data-value to process values
+            button.addEventListener("click", (e) => processValue(e));
             row.appendChild(button);
-            keys[0][i][j] = button; // replace keys array with reference to buttons
         }
         LEFT.append(row); // add row to calc
     }
@@ -85,11 +89,131 @@ function generateButtons(){
         let button = document.createElement("div");
         button.classList.add("button");
         button.innerHTML = keys[1][i];
+        button.dataset.value = keys[1][i]; // use data-value to process values
+        button.addEventListener("click", (e) => processValue(e));
         row.appendChild(button);
-        keys[1][i] = button; // replace keys array with reference to buttons
     }
-
     RIGHT.appendChild(row);
-
-    return keys;
 }
+function processValue(e){
+    // process event accordingly
+    e.stopPropagation();
+
+    let val;
+    if(e.type == "click"){
+        val = e.target.dataset.value;
+    }else{
+        val = e.key;
+    }
+    // if integer
+    if(!isNaN(parseInt(val))){
+        if (operator){
+            secondOperand += val;
+        }
+        else if(!compound){
+            firstOperand += val;
+        }
+    }
+    // if decimal
+    else if(val == "."){ 
+        if(operator){
+            if(secondOperand.indexOf(val) == -1){
+                secondOperand += val;
+            }
+        }
+        else if(firstOperand.indexOf(val) == -1){
+            firstOperand += val;
+            }
+    }
+    // any other operator assuming that an operand has been defined
+    else if (firstOperand){
+        switch(val){
+            case "+":
+                operator = val;
+                break;
+            case "-": 
+                operator = val;
+                break;
+            case "*": 
+                operator = val;
+                break;
+            case "/": 
+                operator = val;
+                break;
+            case "^x":
+                operator = "^";
+                break;
+            case "!":
+                if(!operator){
+                    ans = factorial(firstOperand);
+                    makeCompoundExpression();
+                }
+                break;
+            case "Backspace":
+            case "C":
+                if(secondOperand){
+                    secondOperand = "";
+                }
+                else if(operator){
+                    operator = false;
+                }
+                else{
+                    reset();
+                }
+                break;
+            case "AC":
+                reset();
+                break;
+            case "Enter":
+            case "=":
+                if(secondOperand){
+                    ans = operate(parse(firstOperand), parse(secondOperand), operator);
+                }else{
+                    ans = parse(firstOperand);
+                }
+                makeCompoundExpression();
+                break;
+        }
+    }
+    updateDisplay();
+}
+
+function reset(){
+    firstOperand = "";
+    secondOperand = "";
+    operator = false;
+    compound = false;
+}
+
+function makeCompoundExpression(){
+    firstOperand = ans;
+    secondOperand = "";
+    operator = false;
+    compound = true;
+}
+
+function updateDisplay(){
+
+    if(secondOperand){
+        display = firstOperand + operator + secondOperand;
+    }
+    else if(operator){
+        display = firstOperand + operator;
+    }
+    else if(firstOperand){
+        display = firstOperand;
+    }else{
+        display = "";
+    }
+    SCREEN.innerHTML = display;
+}
+
+function parse(str){
+    str = str.toString();
+    if(str.indexOf(".") >= 0){
+        return parseFloat(str);
+    }
+    return parseInt(str);
+}
+// driver code
+generateButtons();
